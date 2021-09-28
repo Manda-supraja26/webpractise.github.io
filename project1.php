d8c7d605748facf21e8204f29b61813bb70b7de8
Manda-supraja26 <88205562+Manda-supraja26@users.noreply.github.com>
2021-09-28 22:08:03 +0530

    practice

---
+++ project1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>internshala</title>
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+    <nav id="navbar">
+    <div id='logo'>
+        <img src='img\Tech-hub-innovation-logo-1.jpg' alt='myonlineshop.com'>
+    </div>
+
+    <ul>
+        <li class="item"> <a href=""> book</a></li>
+        <li class="item"> <a href=""> experts</a></li>
+        <li class="item"> <a href=""> experts</a></li>
+        <li class="item"> <a href=""> experts</a></li>
+        <li class="item"> <a href=""> experts</a></li>
+    </ul>
+    </nav>
+
+    <!-- here is the body  -->
+    
+    <div class="head">
+        
+       <h1>Get help. Get better.</h1>
+       <p>No matter what's troubling you, get the support you need, right here, right now.</p>
+       <div class="button">
+           <a href="">chat now</a>
+           <a href="">book appointment</a>
+       </div>
+
+</div>
+
+</body>
+</html>
